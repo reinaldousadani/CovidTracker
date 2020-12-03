2e9fb03e31a3256bedd6e5782c20c6b2bfbc3152
Reinaldo Usadani <reinaldo.vanrulloff.96@gmail.com>
2020-12-03 08:02:21 +0800

    update readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { GlobalStyle } from "./globalStyles";
-import Topnav from "./components/Topnav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Countries from "./pages/Countries";
 import Global from "./pages/Global";
